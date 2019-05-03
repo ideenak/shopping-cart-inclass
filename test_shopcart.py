ed6e86227ed27951fb3eb7a84ee55da3efbf2203
ideenak <45948963+ideenak@users.noreply.github.com>
2019-05-03 15:37:47 -0400

    Update test_shopcart.py

--- test_shopcart.py
+++ test_shopcart.py
@@ -1,9 +1,24 @@
-from shopping_cart import to_usd
-from shopping_cart import findprod
+from shopping_cart import to_usd, findprod
 
 
 def test_to_usd():
-    assert True
+    result = to_usd(10)
+    assert result == "$ 10.00"
 
 def test_findprod():
-    assert True
+    
+    products = [
+        {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    ]
+
+
+    idnum_L = []
+    
+    idnum = 3
+        
+    idnum_L.append(idnum)
+    sum_price = 0
+    for i in idnum_L:
+        result = findprod(i, sum_price, products,idnum)
+    
+    assert result == 2.49
