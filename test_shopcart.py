bab63b528cf4924094080360e56d093240fab7de
ideenak <45948963+ideenak@users.noreply.github.com>
2019-05-03 18:40:06 -0400

    Update test_shopcart.py

--- test_shopcart.py
+++ test_shopcart.py
@@ -3,7 +3,13 @@ from shopping_cart import to_usd, findprod
 
 def test_to_usd():
     result = to_usd(10)
+    result1 = to_usd(1000)
+    result2 = to_usd(57.3333)
+    result3 = to_usd(.011111111)
     assert result == "$ 10.00"
+    assert result1 == "$ 1000.00"
+    assert result2 == "$ 57.33"
+    assert result3 == "$ 0.01"
 
 def test_findprod():
     
